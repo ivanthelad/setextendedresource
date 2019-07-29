8891bdb0f39229b7530aa980e0b371c8b1144496
ivanthelad <ivanthelad>
2019-07-29 18:13:56 +0200

    inital

--- src/main/java/com/example/setextendedresource/SetextendedresourceApplication.java
+++ src/main/java/com/example/setextendedresource/SetextendedresourceApplication.java
@@ -4,13 +4,15 @@ import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
 import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
-import io.kubernetes.client.models.V1Pod;
-import io.kubernetes.client.models.V1PodList;
+import io.kubernetes.client.custom.Quantity;
+import io.kubernetes.client.models.*;
 import io.kubernetes.client.util.ClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootApplication
 public class SetextendedresourceApplication {
@@ -33,6 +35,12 @@ public class SetextendedresourceApplication {
 		CoreV1Api api = new CoreV1Api();
 
 		// invokes the CoreV1Api client
+		V1NodeList list2 = api.listNode(null, null, null, null, null, null, null, null, false);
+		for (V1Node item : list2.getItems()) {
+			System.out.println("mynode "+item.getMetadata().getName());
+			item.getStatus().putCapacityItem("myval" ,new Quantity(new BigDecimal(10), Quantity.Format.DECIMAL_SI));
+
+		}
 		V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, false);
 		for (V1Pod item : list.getItems()) {
 			System.out.println(item.getMetadata().getName());
